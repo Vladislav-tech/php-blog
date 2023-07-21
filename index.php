3745bc5d60de8473a1b8336442179f0b8f7a2165
vladislav <vladislav.frint@gmail.com>
2023-07-21 20:55:12 +0300

    git init

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
+
+// Create app
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Max'
+    ]);
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
+    return $response;
+});
+
+$app->run();
